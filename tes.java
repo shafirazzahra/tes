d7414ab886e8e4a2f0fdb218208ad06e7e757a53
shafirazzahra <shafira_azzahra_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:52:02 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,8 @@
+public class tes {
+    public static void main(String[] args) {
+    System.out.println("nama: Shafira Setia Adnari Azzahra");
+    System.out.println("alamat: Blitar");
+    System.out.println("SMP: Smp Negeri 2 Srengat");
+    System.out.println("No HP: 085730162754");
+    }
+}
